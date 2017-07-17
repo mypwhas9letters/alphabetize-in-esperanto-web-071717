802465cee9e8ad590f2b2e3f58a22e92b4004df1
Hui Wang <huiw3939@gmail.com>
2017-07-16 23:17:05 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  esp = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |x|
+    x.split("").map do |i|
+      esp.split("").index(i)
+end
+end
 end
